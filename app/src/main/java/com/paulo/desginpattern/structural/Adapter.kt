2d5c55bcf5e8b32924c1870ff1df9332a3dc4f17
paulosoujava <paulosoujava@gmail.com>
2022-03-10 08:50:27 -0300

    adapter

---
+++ app/src/main/java/com/paulo/desginpattern/structural/Adapter.kt
@@ -0,0 +1,80 @@
+package com.paulo.desginpattern.structural
+
+import kotlin.math.sqrt
+
+/*
+TA\ambém conhecido como: adaptador, wrapper
+o Adapter é um padrão de projeto estrutural que permite objetos com interfaces
+incompatíveis colcaborarem entre si
+ */
+/*
+Digamos que vocÊ tenha classes com interfaces compatíveir:
+RoundHole ( buraco redondo)
+RoundPeg ( Pino redondo )
+ */
+
+class RoundHole(radius: Int = 0) {
+    fun getRadius(): Double {
+        TODO()
+    }//retorna o raio do buraco
+
+    fun fits(peg: RoundPeg): Boolean {
+        TODO()
+    }
+}
+
+open class RoundPeg(width: Int = 0) {
+
+    open fun getRadius(): Double {
+        TODO()
+    }
+}
+
+// MAs tem uma classe incompatível: SquarePeg ( Pino Quadrado)
+class SquarePeg(width: Int) {
+    fun getWidth(): Int {
+        TODO()
+        //retorna a largura do pino quadrado
+    }
+
+}
+/*
+uma classe adaptadora permite que vc encaixe pinos quadrados em buracos redondos
+ela estende a classe roundedPeg para permitir que objetos do adaptador ajam como pinos
+redondos
+*/
+
+class SquarePegAdapter(val peg: SquarePeg) : RoundPeg() {
+    /*
+    na verdade o adaptador contem uma instância da classe SquarePeg
+     */
+
+    /*
+       O adaptador finge que é um pino redondo com um raio que encaixaria
+       o pino quadrado que o adaptador está envolvendo
+        */
+    override fun getRadius() = peg.getWidth() * sqrt(2.0) / 2
+
+
+    /*
+    em algum lugar do código cliente
+     */
+    val hole = RoundHole(5)
+
+    val rpeg = RoundPeg(10)
+    val small_sqpeg = SquarePeg(5)
+    val large_sqpeg = SquarePeg(10)
+
+    val small_sqpeg_adt = SquarePegAdapter(small_sqpeg)
+    val large_sqpeg_adt = SquarePegAdapter(large_sqpeg)
+
+    init {
+        hole.fits(rpeg)
+
+        //isso nao vai compilar ( tipos incompativeis)
+        //hole.fits(small_sqpeg)
+
+        //uando o adaptador para compilar o que acima não nos permitia
+        hole.fits(small_sqpeg_adt)
+    }
+}
---
+++ app/src/main/java/com/paulo/desginpattern/structural/Adapter_01.kt
@@ -0,0 +1,53 @@
+package com.paulo.desginpattern.structural
+
+/*
+Converts the interface of a class into another interface the client expects
+Convert data from one format int another
+Used extensively in android
+ */
+
+data class  DisplayDataType( val index: Float, val data: String)
+
+class DataDisplay{
+    fun displayData( data: DisplayDataType){
+        println("Data is displayed ${data.index} - ${data.data}")
+    }
+}
+
+data class DatabaseData(val position: Int, val amount: Int)
+
+class DatabaseGenerator{
+    fun generateData(): List<DatabaseData>{
+        val list = arrayListOf<DatabaseData>()
+        list.add( DatabaseData(1,2))
+        list.add( DatabaseData(2,23))
+        list.add( DatabaseData(3,4))
+        list.add( DatabaseData(4,5))
+
+        return list
+    }
+}
+
+
+interface DatabaseDataConverter{
+    fun convertData(data: List<DatabaseData>): List<DisplayDataType>
+}
+
+class DataDisplayAdapter(val display: DataDisplay): DatabaseDataConverter{
+    override fun convertData(data: List<DatabaseData>): List<DisplayDataType> {
+        val returnList = arrayListOf<DisplayDataType>()
+        data.forEach {
+            val ddt = DisplayDataType(it.position.toFloat(), it.amount.toString())
+            returnList.add(ddt)
+            display.displayData(ddt)
+        }
+        return returnList
+    }
+}
+
+fun main() {
+    val generate = DatabaseGenerator()
+    val generatedData = generate.generateData()
+    val adapter = DataDisplayAdapter(DataDisplay())
+    val convertData = adapter.convertData(generatedData)
+}
